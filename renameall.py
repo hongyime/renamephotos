f32e243095990094b70a63b9288b8c66c5ec4343
Bryan <hongyime@users.noreply.github.com>
2022-06-04 13:33:18 +0800

    Update renameall.py

--- renameall.py
+++ renameall.py
@@ -7,12 +7,16 @@ for count, filename in enumerate(os.listdir(folder)):
                 ext = '.jpg'
         elif filename.endswith('.jpeg'):
                 ext = '.jpeg'
-        elif filename.endswith('.mov'):
-                ext = '.mov'
-        elif filename.endswith('.mp4'):
-                ext = '.mp4'
-        elif filename.endswith('.mp3'):
-                ext = '.mp3'
+        elif filename.endswith('.png'):
+                ext = '.png'
+        elif filename.endswith('.JPG'):
+                ext = '.JPG'
+#         elif filename.endswith('.mov'):
+#                 ext = '.mov'
+#         elif filename.endswith('.mp4'):
+#                 ext = '.mp4'
+#         elif filename.endswith('.mp3'):
+#                 ext = '.mp3'
         else:
                 continue
         rand = "".join(random.sample(string.ascii_lowercase + string.digits, 20)) + ext
